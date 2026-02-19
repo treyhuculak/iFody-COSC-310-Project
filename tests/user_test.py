42472508311384de7cdff4eb6a2da6356a67b166
LostInHustle <dystopiandice@outlook.com>
2026-02-18 18:20:47 -0800

    Updated the naming convention to prevent conflicts (user -> usr).

--- tests/user_test.py
+++ tests/user_test.py
@@ -5,12 +5,12 @@ def test_user_with_valid_email() -> None:
     '''
     Tests the User class constructor with a valid email address.
     '''
-    user = user.User('TempUser', 'tempuser@tu.com', '1234', 'customer', False)
-    assert user == "User('TempUser', 'tempuser@tu.com', '1234', 'customer', False)"
+    usr = user.User('TempUser', 'tempuser@tu.com', '1234', 'customer', False)
+    assert usr == "User('TempUser', 'tempuser@tu.com', '1234', 'customer', False)"
 
 def test_user_with_invalid_email() -> None:
     '''
     Tests the User class constructor with an invalid email address.
     '''
     with pytest.raises(ValueError):
-        user = user.User('TempUser', '', '1234', 'customer', False)
+        _ = user.User('TempUser', '', '1234', 'customer', False)
