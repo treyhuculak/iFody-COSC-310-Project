a35dabadc9caf403f953e17eff34b4a8e1d0995f
LostInHustle <dystopiandice@outlook.com>
2026-02-18 17:54:41 -0800

    Implemented an initial draft of the User class, including its getters and setters.

--- src/backend/models/user.py
+++ src/backend/models/user.py
@@ -26,3 +26,52 @@ class User:
         self.__password = password
         self.__role = role
         self.__is_blocked = is_blocked
+
+    def __repr__(self) -> str:
+        '''
+        Returns a demonstration string for debugging purposes.
+        '''
+        return "User(%r, %r, %r, %r, %r)" % (
+            self.__username,
+            self.__emailaddress,
+            self.__password,
+            self.__role,
+            self.__is_blocked
+        )
+    
+    def get_username(self) -> str:
+        '''
+        Returns the value of the username attribute.
+        '''
+        return self.__username
+    
+    def get_emailaddress(self) -> str:
+        '''
+        Returns the value of the emailaddress attribute.
+        '''
+        return self.__emailaddress
+    
+    def get_password(self) -> str:
+        '''
+        Returns the value of the password attribute.
+        '''
+        return self.__password
+    
+    def get_role(self) -> str:
+        '''
+        Returns the value of the role attribute.
+        '''
+        return self.__role
+    
+    def get_is_blocked(self) -> str:
+        '''
+        Returns the value of the is_blocked attribute.
+        '''
+        return self.__is_blocked
+    
+    def _set_is_blocked(self, status: bool) -> None:
+        '''
+        Sets the user's status to blocked (True) or unblocked (False).
+        The function is hidden as we assume only admin accounts can use it.
+        '''
+        self.__is_blocked = status
