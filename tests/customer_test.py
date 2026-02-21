b0fde4372a4dbd779fdcd9f50e9d6be4250d56ac
LostInHustle <dystopiandice@outlook.com>
2026-02-20 20:07:04 -0800

    Remove the is_blocked parameter from the Customer initializer to ensure safety.

--- tests/customer_test.py
+++ tests/customer_test.py
@@ -5,18 +5,18 @@ def test_customer_login_with_valid_email() -> None:
     '''
     Tests the Customer class when attempting to log in with a valid email address.
     '''
-    customer = Customer('TempUser', 'tempuser@tu.com', 'Abc@1234', False)
+    customer = Customer('TempUser', 'tempuser@tu.com', 'Abc@1234')
     customer.login()
 
 def test_customer_login_with_invalid_email() -> None:
     '''
     Tests the Customer class when attempting to log in with an invalid email address.
     '''
-    customer = Customer('TempUser', '', 'Abc@1234', False)
+    customer = Customer('TempUser', '', 'Abc@1234')
     with pytest.raises(ValueError):
         customer.login()
 
-    customer = Customer('TempUser', '', 'OhYeah', False)
+    customer = Customer('TempUser', '', 'OhYeah')
     with pytest.raises(ValueError):
         customer.login()
 
@@ -24,17 +24,17 @@ def test_customer_login_with_valid_password() -> None:
     '''
     Tests the Customer class when attempting to log in with a valid password.
     '''
-    customer = Customer('TempUser', 'tempuser@tu.com', 'Abc@1234', False)
+    customer = Customer('TempUser', 'tempuser@tu.com', 'Abc@1234')
     customer.login()
 
 def test_customer_login_with_valid_password() -> None:
     '''
     Tests the Customer class when attempting to log in with an invalid password.
     '''
-    customer = Customer('TempUser', 'tempuser@tu.com', '', False)
+    customer = Customer('TempUser', 'tempuser@tu.com', '')
     with pytest.raises(ValueError):
         customer.login()
 
-    customer = Customer('TempUser', 'tempuser@tu.com', '1234', False)
+    customer = Customer('TempUser', 'tempuser@tu.com', '1234')
     with pytest.raises(ValueError):
         customer.login()
