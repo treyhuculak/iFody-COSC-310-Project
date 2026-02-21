8eae73f5019cc2587e839126293a00bbf7b12165
LostInHustle <dystopiandice@outlook.com>
2026-02-20 19:54:11 -0800

    Refactor customer_test.py to apply the Control Variable Method when testing login behavior with valid and invalid email and password inputs.

--- tests/customer_test.py
+++ tests/customer_test.py
@@ -1,17 +1,40 @@
 import pytest
-from src.backend.models import user
+from src.backend.models.user import Customer
 
-def test_user_with_valid_email() -> None:
+def test_customer_login_with_valid_email() -> None:
     '''
-    Tests the User class constructor with a valid email address.
+    Tests the Customer class when attempting to log in with a valid email address.
     '''
-    usr = user.User('TempUser', 'tempuser@tu.com', '1234', 'customer', False)
-    assert str(usr) == "User('TempUser', 'tempuser@tu.com', '1234', 'customer', False)"
-    assert repr(usr) == "User('TempUser', 'tempuser@tu.com', '1234', 'customer', False)"
+    customer = Customer('TempUser', 'tempuser@tu.com', 'Abc@1234', False)
+    customer.login()
 
-def test_user_with_invalid_email() -> None:
+def test_customer_login_with_invalid_email() -> None:
     '''
-    Tests the User class constructor with an invalid email address.
+    Tests the Customer class when attempting to log in with an invalid email address.
     '''
+    customer = Customer('TempUser', '', 'Abc@1234', False)
     with pytest.raises(ValueError):
-        _ = user.User('TempUser', '', '1234', 'customer', False)
+        customer.login()
+
+    customer = Customer('TempUser', '', 'OhYeah', False)
+    with pytest.raises(ValueError):
+        customer.login()
+
+def test_customer_login_with_valid_password() -> None:
+    '''
+    Tests the Customer class when attempting to log in with a valid password.
+    '''
+    customer = Customer('TempUser', 'tempuser@tu.com', 'Abc@1234', False)
+    customer.login()
+
+def test_customer_login_with_valid_password() -> None:
+    '''
+    Tests the Customer class when attempting to log in with an invalid password.
+    '''
+    customer = Customer('TempUser', 'tempuser@tu.com', '', False)
+    with pytest.raises(ValueError):
+        customer.login()
+
+    customer = Customer('TempUser', 'tempuser@tu.com', '1234', False)
+    with pytest.raises(ValueError):
+        customer.login()
