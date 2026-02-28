40ca932d146b9215fe14eb27c2841d3c3a145745
LostInHustle <dystopiandice@outlook.com>
2026-02-28 12:08:14 -0800

    Add a userv2.py module that refactors the User, Admin, Customer, and RestaurantOwner classes using the Pydantic library.

---
+++ src/backend/models/userv2.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel, Field
+
+email_pattern = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}"
+password_pattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$#%])[A-Za-z\\d@$#%]{8,10}$"
+role_pattern = "^(Admin|Customer|RestaurantOwner)$"
+
+class UserBase(BaseModel):
+    username: str = Field(..., min_length = 1)
+    emailaddress: str = Field(..., pattern = email_pattern)
+    password: str = Field(..., pattern = password_pattern)
+    role: str = Field(..., pattern = role_pattern)
+    is_logged_in: bool = False
+    is_blocked: bool = False
+
+class Admin(UserBase):
+    role: str = 'Admin'
+    is_logged_in: bool = True
+
+class Customer(UserBase):
+    role: str = 'Customer'
+    is_logged_in: bool = True
+
+class RestaurantOwner(UserBase):
+    role: str = 'RestaurantOwner'
+    is_logged_in: bool = True
