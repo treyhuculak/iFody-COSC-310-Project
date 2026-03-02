069b126f555659b7db29ac5b5738b8a451bdfd47
LostInHustle <dystopiandice@outlook.com>
2026-03-02 14:38:36 -0800

    Changing the class name CustomerRepository to UserRepository, and updating the corresponding function names to ensure they are fully generalized.

--- src/backend/repositories/user_repo.py
+++ src/backend/repositories/user_repo.py
@@ -1,47 +1,47 @@
 import json
 
-class CustomerRepository:
-    CUSTOMER_FILE = 'data/customer_db.json'
+class UserRepository:
+    USER_FILE = 'data/user_db.json'
     
     def __init__(self):
         '''
-        Checks if data/customer_db.json exists. If not, creates it and writes the headers.
+        Checks if data/user_db.json exists. If not, creates it and writes the headers.
         '''
         pass
     
-    def get_all_customers(self):
+    def get_all_users(self):
         '''
-        Returns all the Customer(s) from the database.
+        Returns all the Admin/Customer/RestaurantOwner instances from the database.
         '''
-        with open(self.CUSTOMER_FILE) as file:
-            customers = json.load(file)['Customers']
-            return customers
+        with open(self.USER_FILE) as file:
+            users = json.load(file)['Users']
+            return users
     
-    def get_customer_by_id(self):
+    def get_user_by_id(self):
         '''
-        Returns the Customer instance according to the id.
+        Returns the Admin/Customer/RestaurantOwner instance according to the id.
         '''
         return []
     
-    def get_customer_by_username(self, username):
+    def get_user_by_username(self, username):
         '''
-        Returns the Customer instance according to the username.
+        Returns the Admin/Customer/RestaurantOwner instance according to the username.
         '''
-        with open(self.CUSTOMER_FILE) as file:
-            customers = json.load(file)['Customers']
-            for row in customers:
+        with open(self.USER_FILE) as file:
+            users = json.load(file)['Users']
+            for row in users:
                 if row['username'] == username :
                     return row
             return None
 
     
-    def get_customer_by_email(self, email):
+    def get_user_by_email(self, email):
         '''
-        Returns the Customer instance according to the email.
+        Returns the Admin/Customer/RestaurantOwner instance according to the email.
         '''
-        with open(self.CUSTOMER_FILE) as file:
-            customers = json.load(file)['Customers'] 
-            for row in customers:
+        with open(self.USER_FILE) as file:
+            users = json.load(file)['Users'] 
+            for row in users:
                 if row['email'] == email :
                     return row
             return None
