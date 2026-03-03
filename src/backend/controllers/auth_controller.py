884384e0f2bbc80f24aa9af2a42b84eb7fb92ff8
LostInHustle <dystopiandice@outlook.com>
2026-03-03 10:37:56 -0800

    Adding future plans and type hints for better readability.

--- src/backend/controllers/auth_controller.py
+++ src/backend/controllers/auth_controller.py
@@ -1,5 +1,24 @@
-from typing import Union
+from typing import Optional, Union
+import json
 
 from fastapi import HTTPException
 from src.backend.models.user import Admin, Customer, RestaurantOwner
 from src.backend.repositories.user_repo import UserRepository
+
+
+class AuthController:
+    def __init__(self, repo: Optional[UserRepository] = None) -> None:
+        self.repo = repo or UserRepository()
+
+    def register(self, username: str, email: str, password: str, role: Union[Admin, Customer, RestaurantOwner]):
+        # TODO — We need to add the corresponding methods to the UserRepository.
+        pass
+        
+    def login(self, email: str, password: str):
+        user_info = self.repo.get_user_by_email(email)
+        if user_info == None:
+            raise HTTPException(status_code = 404, detail = "The account does not exist in the system.")
+        if password == user_info['password']:
+              return user_info
+        else:
+            raise HTTPException(status_code = 400, detail = "The given password is not correct.")
