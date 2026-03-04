4fdc16065006d14b138ff783a51f5ae7b63bcb28
LostInHustle <dystopiandice@outlook.com>
2026-03-03 17:55:35 -0800

    Rename the functions in auth_controller_test.py to ensure the names accurately describe their behavior, and add docstrings.

--- tests/auth_controller_test.py
+++ tests/auth_controller_test.py
@@ -3,16 +3,33 @@ from fastapi import HTTPException
 from src.backend.controllers.auth_controller import AuthController
 from src.backend.repositories.user_repo import UserRepository
 
-repo = UserRepository
 controller = AuthController()
 
 def test_valid_login() -> None:
-    assert (controller.login("testcustomer@123.com", "Test@123") != None)
+    '''
+    Tests the login functionality using an existing email with the correct password.
+    '''
+    assert controller.login("testcustomer@123.com", "Test@123") != None
+    assert controller.login("testcustomer@123.com", "Test@123") == {
+        "id": 1,
+        "username": "TestCustomer",
+        "email": "testcustomer@123.com",
+        "password": "Test@123",
+        "role": "Customer",
+        "is_logged_in": False,
+        "is_blocked": False
+    }
 
 def test_invalid_login() -> None:
+    '''
+    Tests the login function with a nonexistent email and an incorrect password.
+    '''
     with pytest.raises(HTTPException):
         controller.login("fakeuser@111.com", "fakePassword")
 
-def test_validemail_invalidpass() -> None:
+def test_login_invalid_password() -> None:
+    '''
+    Tests the login functionality using a valid email with an incorrect password.
+    '''
     with pytest.raises(HTTPException):
         controller.login("testcustomer@123.com", "fakePassword")
