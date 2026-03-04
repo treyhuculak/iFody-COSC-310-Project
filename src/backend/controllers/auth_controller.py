5e402bb8e69aa68a1963f7a6f4a4bc29e37ea732
LostInHustle <dystopiandice@outlook.com>
2026-03-04 09:39:28 -0800

    Add docstrings for the functions in auth_controller.py.

--- src/backend/controllers/auth_controller.py
+++ src/backend/controllers/auth_controller.py
@@ -6,12 +6,21 @@ from src.backend.repositories.user_repo import UserRepository
 
 class AuthController:
     def __init__(self, repo: Optional[UserRepository] = None) -> None:
+        '''
+        Initializes the AuthController class with the necessary fields.
+        '''
         self.repo = repo or UserRepository()
 
     def register(self, username: str, email: str, password: str, role: str):
+        '''
+        Creates an account instance and saves it to the database when the email, password, and role are all valid.
+        '''
         pass
         
     def login(self, email: str, password: str):
+        '''
+        Logs in using the email and password given.
+        '''
         user_info = self.repo.get_user_by_email(email)
         if user_info == None:
             raise HTTPException(status_code = 404, detail = "The account is not found.")
