45ad953507602330ae8518f0d31a4ffeaca1041a
LostInHustle <dystopiandice@outlook.com>
2026-03-04 09:33:13 -0800

    Refactor user.py with more detailed and categorized error messages, along with correcting the customer_test.py file.

--- src/backend/models/user.py
+++ src/backend/models/user.py
@@ -5,14 +5,35 @@ email_pattern = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}"
 password_pattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$#%])[A-Za-z\\d@$#%]{8,10}$"
 role_pattern = "^(Admin|Customer|RestaurantOwner)$"
 
+class InvalidEmailError(Exception):
+    pass
+
+class InvalidPasswordError(Exception):
+    pass
+
+class InvalidRoleError(Exception):
+    pass
+
 class UserBase(BaseModel):
     id: int = Field(..., ge = 1)
     username: str = Field(..., min_length = 1)
-    email: str = Field(..., pattern = email_pattern)
+    email: str = Field(..., min_length = 1)
     password: str = Field(..., min_length = 1)
-    role: str = Field(..., pattern = role_pattern)
+    role: str = Field(..., min_length = 5)
     is_logged_in: bool = False
     is_blocked: bool = False
+
+    @field_validator('email')
+    def check_email(cls, email_input: str) -> str:
+        result = re.fullmatch(
+            email_pattern,
+            email_input
+        )
+
+        if result:
+            return result
+        else:
+            raise InvalidEmailError('The email address is in an invalid format.')
 
     @field_validator('password')
     def check_password(cls, password_input: str) -> str:
@@ -24,7 +45,19 @@ class UserBase(BaseModel):
         if result:
             return result
         else:
-            raise ValueError('The password must contain at least one number, one uppercase letter, one lowercase letter, one special character, and be between 8 and 10 characters long.')
+            raise InvalidPasswordError('The password must contain at least one number, one uppercase letter, one lowercase letter, one special character, and be between 8 and 10 characters long.')
+        
+    @field_validator('role')
+    def check_role(cls, role_input: str) -> str:
+        result = re.fullmatch(
+            role_pattern,
+            role_input
+        )
+
+        if result:
+            return result
+        else:
+            raise InvalidRoleError('The role must be one of: Admin, Customer, or RestaurantOwner.')
 
 class Admin(UserBase):
     role: str = 'Admin'
--- tests/customer_test.py
+++ tests/customer_test.py
@@ -1,5 +1,5 @@
-import pytest
-from src.backend.models.user import Customer
+import pytest, pydantic_core
+from src.backend.models.user import Customer, InvalidEmailError, InvalidPasswordError
 
 def test_customer_with_valid_email() -> None:
     '''
@@ -11,11 +11,11 @@ def test_customer_with_invalid_email() -> None:
     '''
     Tests the Customer class when attempting to log in with an invalid email address.
     '''
-    with pytest.raises(ValueError):
+    with pytest.raises(pydantic_core.ValidationError):
         customer = Customer(id = 1, username = 'TempUser', email = '', password = 'Abc@1234')
 
-    with pytest.raises(ValueError):
-        customer = Customer(id = 1, username = 'TempUser', email = '', password = 'OhYeah')
+    with pytest.raises(InvalidEmailError):
+        customer = Customer(id = 1, username = 'TempUser', email = 'RandomString', password = 'OhYeah')
 
 def test_customer_with_valid_password() -> None:
     '''
@@ -27,8 +27,8 @@ def test_customer_with_invalid_password() -> None:
     '''
     Tests the Customer class when attempting to log in with an invalid password.
     '''
-    with pytest.raises(ValueError):
+    with pytest.raises(pydantic_core.ValidationError):
         customer = Customer(id = 1, username = 'TempUser', email = 'tempuser@tu.com', password = '')
 
-    with pytest.raises(ValueError):
+    with pytest.raises(InvalidPasswordError):
         customer = Customer(id = 1, username = 'TempUser', email = 'tempuser@tu.com', password = '1234')
