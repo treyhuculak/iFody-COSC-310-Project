7b5ed4764117109135890a1b6fe26bb5121a54d3
Trey Huculak <144386330+treyhuculak@users.noreply.github.com>
2026-03-08 16:34:30 -0700

    Get delivery fee, tax, subtotal, grand total (#76)
    * Saving my work
    
    * Order get delivery fee, tax, subtotal, and grand total
    
    * Made necessary fixes for falied tests
    
    * Fixed bugs involved with the merge

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 
 from src.backend.routers import restaurants
-from src.backend.routers import order
+from src.backend.routers import orders
 
 app = FastAPI()
 
 app.include_router(restaurants.router)
-app.include_router(order.router)
+app.include_router(orders.router)
 
 @app.get("/")
 def root():
