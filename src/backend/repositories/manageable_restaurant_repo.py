e4e73bcf46665748ebc7539abe782ef1617ce735
LostInHustle <dystopiandice@outlook.com>
2026-03-10 11:33:05 -0700

    Make sure the necessary repositories are injected.

--- src/backend/repositories/manageable_restaurant_repo.py
+++ src/backend/repositories/manageable_restaurant_repo.py
@@ -9,4 +9,9 @@ class ManageableRestaurantRepository:
         '''
         Initializes a ManageableRestaurantRepository instance using the user_repo_file and rest_repo_file arguments.
         '''
-        pass
+        # We check whether alternative file paths are given, otherwise, we use the default paths.
+        user_repo_file = user_repo_file or "data/user_db.json"
+        rest_repo_file = rest_repo_file or "data/restaurants.json"
+
+        self.user_repo = user_repo.UserRepository(user_repo_file)
+        self.rest_repo = restaurant_repo.RestaurantRepository(rest_repo_file)
