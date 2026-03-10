eace249bbed9d4e4fc8f92b6f6945ccd9c0fba98
jhlbolsonaro <113214459+jhlbolsonaro@users.noreply.github.com>
2026-03-10 10:13:55 -0700

    Feature/notifications (#83)
    * added notif json, notif model, repo (still to be completed), modified user.py in model to include enumerator, and fixed small issue in order_repo
    
    * finalized notification repository, added notif_controller file
    
    * finished notification controller
    
    * finished notification router, and changed the file names for consistency.
    
    * added router to main.py and fixed notification controller
    
    * added notification creation upon status changes in the order controller, added a new Notification type for failed payment, and deleted one unnecessary notification type
    
    * created new notification type, and modified add_order function in order controller to create notifications when new order is created
    
    * fixed some tests
    
    * fixed the tests that were having errors, basically fixing role enumerator issues
    
    * fixed the error of using the real notification json file during tests

--- src/backend/main.py
+++ src/backend/main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 
 from src.backend.routers import restaurants
 from src.backend.routers import orders
+from src.backend.routers import notification
 
 app = FastAPI()
 
 app.include_router(restaurants.router)
 app.include_router(orders.router)
+app.include_router(notification.router)
 
 @app.get("/")
 def root():
--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,8 @@ from src.backend.main import app
 from src.backend.routers.orders import get_controller
 from src.backend.repositories.order_repo import OrderRepository
 from src.backend.controllers.order_controller import OrderController
+from src.backend.repositories.notification_repo import NotificationRepository
+from src.backend.controllers.notification_controller import NotificationController
 
 @pytest.fixture
 def test_client(tmp_path):
@@ -16,9 +18,14 @@ def test_client(tmp_path):
     temp_db = tmp_path / "test.json"
     temp_db.write_text(json.dumps([]))
 
+    temp_notif_db = tmp_path / "test_notifications.json"
+    temp_notif_db.write_text(json.dumps([]))
+
     test_repo = OrderRepository(file_path=str(temp_db))
-    test_controller = OrderController()
-    test_controller.order_repo = test_repo
+    test_notif_repo = NotificationRepository(file_path=str(temp_notif_db))
+
+    test_notif_controller = NotificationController(repo=test_notif_repo)
+    test_controller = OrderController(repo=test_repo, notif_controller=test_notif_controller)
 
     app.dependency_overrides[get_controller] = lambda: test_controller
 
