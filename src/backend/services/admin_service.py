e497e1d408be04a2be3ec34781b12440096d41a2
LostInHustle <dystopiandice@outlook.com>
2026-03-15 12:35:16 -0700

    Implement get_all_orders and get_most_popular_restaurant functions.

--- src/backend/services/admin_service.py
+++ src/backend/services/admin_service.py
@@ -15,7 +15,7 @@ class AdminService:
         self.rest_repo = RestaurantRepository(rest_repo_file)
         self.order_repo = OrderRepository(order_repo_file)
 
-    def get_all_the_orders(self) -> list[dict]:
+    def get_all_orders(self) -> list[dict]:
         '''
         Gets all the orders from all the restaurant instances.
         '''
@@ -27,7 +27,15 @@ class AdminService:
             orders.append(order)
         return orders
     
-    def get_the_most_popular_restaurant(self) -> dict:
+    def get_most_popular_restaurant(self) -> dict:
         '''
         Retrieves the restaurant instance with the highest number of orders.
         '''
+        rests_and_orders = dict()
+        for order in self.get_all_orders():
+            if order["restaurant_id"] in rests_and_orders:
+                order["restaurant_id"] += 1
+            else:
+                order["restaurant_id"] = 1
+        most_popular_restaurant = max(rests_and_orders, key = rests_and_orders.get)
+        return most_popular_restaurant
