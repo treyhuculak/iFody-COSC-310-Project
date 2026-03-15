a3531f822d629616d60657b9cc4bfaccea860c8d
LostInHustle <dystopiandice@outlook.com>
2026-03-15 12:28:09 -0700

    Implementing the admin_service.py file.

---
+++ src/backend/services/admin_service.py
@@ -0,0 +1,33 @@
+from src.backend.models.user import Role
+from src.backend.repositories.restaurant_repo import RestaurantRepository
+from src.backend.repositories.order_repo import OrderRepository
+import json
+
+class AdminService:
+    def __init__(
+            self,
+            rest_repo_file: str = None,
+            order_repo_file: str = None
+        ):
+        '''
+        Initializes the AdminService class's attributes.
+        '''
+        self.rest_repo = RestaurantRepository(rest_repo_file)
+        self.order_repo = OrderRepository(order_repo_file)
+
+    def get_all_the_orders(self) -> list[dict]:
+        '''
+        Gets all the orders from all the restaurant instances.
+        '''
+        restaurants = self.rest_repo.get_all_restaurants()
+        orders = []
+        for restaurant in restaurants:
+            restaurant_id = restaurant["id"]
+            order = self.order_repo.get_order_by_id(restaurant_id)
+            orders.append(order)
+        return orders
+    
+    def get_the_most_popular_restaurant(self) -> dict:
+        '''
+        Retrieves the restaurant instance with the highest number of orders.
+        '''
