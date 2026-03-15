5c35030973259f2fbabe50d80c2bfd65b12fa8ef
LostInHustle <dystopiandice@outlook.com>
2026-03-15 15:16:11 -0700

    Adding more tests for admin_service.py, something is wrong within the test_nonempty_get_most_popular_restaurant function.

--- src/backend/services/admin_service.py
+++ src/backend/services/admin_service.py
@@ -1,15 +1,17 @@
 from src.backend.repositories.restaurant_repo import RestaurantRepository
 from src.backend.repositories.order_repo import OrderRepository
+from typing import Union
 
 class AdminService:
     def __init__(
-            self
+            self,
+            draft_order_db: str = None
         ):
         '''
         Initializes the AdminService class's attributes.
         '''
         self.rest_repo = RestaurantRepository()
-        self.order_repo = OrderRepository()
+        self.order_repo = OrderRepository(draft_order_db) if draft_order_db else OrderRepository()
 
     def get_all_orders(self) -> list[dict]:
         '''
@@ -24,7 +26,7 @@ class AdminService:
                 orders.append(order)
         return orders
     
-    def get_most_popular_restaurant(self) -> dict | None:
+    def get_most_popular_restaurant(self) -> Union[dict, None]:
         '''
         Retrieves the restaurant instance with the highest number of orders.
         '''
@@ -33,11 +35,13 @@ class AdminService:
         if not orders:
             return None
         for order in orders:
-            if order["restaurant_id"] in rests_and_orders:
-                order["restaurant_id"] += 1
+            restaurant_id = order["restaurant_id"]
+            if restaurant_id in rests_and_orders:
+                rests_and_orders[restaurant_id] += 1
             else:
-                order["restaurant_id"] = 1
+                rests_and_orders[restaurant_id] = 1
         if not rests_and_orders:
             return None
-        most_popular_restaurant = max(rests_and_orders, key = rests_and_orders.get)
+        most_popular_restaurant_id = max(rests_and_orders, key = rests_and_orders.get)
+        most_popular_restaurant = self.rest_repo.get_restaurant_by_id(most_popular_restaurant_id)
         return most_popular_restaurant
--- tests/admin_service_test.py
+++ tests/admin_service_test.py
@@ -1,10 +1,104 @@
+from src.backend.models.order import OrderCreate
 from src.backend.services.admin_service import AdminService
 import pytest, typing
 
-admin_service = AdminService()
+admin_service = None
 
-def test_empty_get_all_orders() -> None:
+@pytest.fixture
+def setup_admin_service() -> None:
+    '''
+    Initializes variable admin_service using the __init__ function of the AdminService class.
+    '''
+    global admin_service
+    admin_service = AdminService()
+
+@pytest.fixture
+def setup_admin_service_with_draft_order_db() -> typing.Generator:
+    '''
+    Initializes variable admin_service and sets up a draft database for orders.
+    '''
+    global admin_service
+    admin_service = AdminService("data/draft_order_db.json")
+    yield
+    import os
+    os.remove(os.getcwd() + "/data/draft_order_db.json")
+
+def test_empty_get_all_orders(setup_admin_service) -> None:
+    '''
+    Tests the get_all_orders function when there are no orders in the database.
+    '''
     assert admin_service.get_all_orders() == []
 
-def test_empty_get_most_popular_restaurant() -> None:
+def test_empty_get_most_popular_restaurant(setup_admin_service) -> None:
+    '''
+    Tests the get_most_popular_restaurant function when there are no orders in the database.
+    '''
     assert admin_service.get_most_popular_restaurant() == None
+
+def test_nonempty_get_all_orders(setup_admin_service_with_draft_order_db) -> None:
+    '''
+    Tests the get_all_orders function before and after creating orders in the draft database.
+    '''
+    assert len(admin_service.get_all_orders()) == 0
+    admin_service.order_repo.create_order(
+        {
+            "customer_id": 1,
+            "restaurant_id": 1,
+            "status": "pending",
+            "location": "BC",
+            "order_items": []
+        }
+    )
+    assert len(admin_service.get_all_orders()) == 1
+    admin_service.order_repo.create_order(
+        {
+            "customer_id": 2,
+            "restaurant_id": 2,
+            "status": "pending",
+            "location": "BC",
+            "order_items": []
+        }
+    )
+    assert len(admin_service.get_all_orders()) == 2
+
+def test_nonempty_get_most_popular_restaurant(setup_admin_service_with_draft_order_db) -> None:
+    admin_service.order_repo.create_order(
+        {
+            "customer_id": 1,
+            "restaurant_id": 1,
+            "status": "pending",
+            "location": "BC",
+            "order_items": []
+        }
+    )
+    assert admin_service.get_most_popular_restaurant() != None
+    assert admin_service.get_most_popular_restaurant()["id"] == 1
+    admin_service.order_repo.create_order(
+        {
+            "customer_id": 2,
+            "restaurant_id": 2,
+            "status": "pending",
+            "location": "BC",
+            "order_items": []
+        }
+    )
+    admin_service.order_repo.create_order(
+        {
+            "customer_id": 3,
+            "restaurant_id": 2,
+            "status": "pending",
+            "location": "BC",
+            "order_items": []
+        }
+    )
+    assert admin_service.get_most_popular_restaurant()["id"] == 2
+    admin_service.order_repo.create_order(
+        {
+            "customer_id": 4,
+            "restaurant_id": 1,
+            "status": "pending",
+            "location": "BC",
+            "order_items": []
+        }
+    )
+    assert admin_service.get_most_popular_restaurant()["id"] == 1
