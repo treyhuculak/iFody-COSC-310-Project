22c5c507d6b7e7f85409b5112a2e7c8cf248bd74
udeluca <umbertodl20@gmail.com>
2026-03-22 00:41:09 -0700

    Last payment service and transaction tests implemented

--- tests/transaction_test.py
+++ tests/transaction_test.py
@@ -53,6 +53,20 @@ card_payment = {
     "name_on_card": "Umberto De Luca"
 }
 
+def test_create_transaction_with_invalid_fields(test_client):
+    # The controller should return the new payment dict, which the router translates to a 200 response
+    payment_response = test_client.post("/payment/cash", params={"active": True}, json=cash_payment) 
+    assert payment_response.status_code == 200
+    payment_id = payment_response.json()["id"]
+
+    transaction_payload = {
+        "payment_method_id": payment_id
+    }
+
+    # Should return 422 since the transaction has not the correct fields for TransactionCreate
+    response = test_client.post("/transaction/", json=transaction_payload)
+    assert response.status_code == 422
+
 def test_create_cash_transaction(test_client):
     # The controller should return the new payment dict, which the router translates to a 200 response
     payment_response = test_client.post("/payment/cash", params={"active": True}, json=cash_payment) 
@@ -76,6 +90,22 @@ def test_create_cash_transaction(test_client):
     assert data["is_successful"] is True
     assert "id" in data
 
+def test_create_invalid_cash_transaction(test_client):
+    # The controller should return the new payment dict, which the router translates to a 200 response
+    payment_response = test_client.post("/payment/cash", params={"active": True}, json=cash_payment) 
+    assert payment_response.status_code == 200
+    payment_id = 999
+
+    transaction_payload = {
+        "payment_method_id": payment_id,
+        "order_id": 15,
+        "amount": 45.50
+    }
+
+    # Since now payment id exists with id == 999, the transaction should give a 500 status code (no payment id was found for payment id == 999)
+    response = test_client.post("/transaction/", json=transaction_payload)
+    assert response.status_code == 500
+
 def test_create_card_transaction(test_client):
     # The controller should return the new payment dict, which the router translates to a 200 response
     payment_response = test_client.post("/payment/card", params={"active": True}, json=card_payment) 
@@ -98,6 +128,22 @@ def test_create_card_transaction(test_client):
     assert data["amount"] == 25.50
     assert data["is_successful"] is True
     assert "id" in data
+
+def test_create_invalid_card_transaction(test_client):
+    # The controller should return the new payment dict, which the router translates to a 200 response
+    payment_response = test_client.post("/payment/card", params={"active": True}, json=card_payment) 
+    assert payment_response.status_code == 200
+    payment_id = 999
+
+    transaction_payload = {
+        "payment_method_id": payment_id,
+        "order_id": 10,
+        "amount": 25.50
+    }
+
+    # Since now payment id exists with id == 999, the transaction should give a 500 status code (no payment id was found for payment id == 999)
+    response = test_client.post("/transaction/", json=transaction_payload)
+    assert response.status_code == 500
 
 def test_delete_card_transaction(test_client):
     # The controller should return the new payment dict, which the router translates to a 200 response
@@ -148,6 +194,13 @@ def test_delete_cash_transaction(test_client):
     # After deletion, trying to get the transaction should return a 404
     get_response = test_client.get(f"/transaction/{transaction_id}")
     assert get_response.status_code == 404
+
+def test_delete_transaction_with_invalid_id(test_client):
+    transaction_id = 999
+
+    # Delete transaction should retrieve 404 since no transaction is found with id == 999
+    delete_response = test_client.delete(f"/transaction/{transaction_id}")
+    assert delete_response.status_code == 404
 
 def test_get_card_transaction_by_id(test_client):
     # The controller should return the new payment dict, which the router translates to a 200 response
@@ -201,6 +254,13 @@ def test_get_cash_transaction_by_id(test_client):
     assert data["is_successful"] == True
     assert "id" in data
 
+def test_get_transaction_by_invalid_id(test_client):
+    transaction_id = 999
+
+    # Now try to retrieve transaction should give a 404 since no transaction exists with that id
+    get_response = test_client.get(f"/transaction/{transaction_id}")
+    assert get_response.status_code == 404
+
 def test_get_all_transactions_by_user_id(test_client):
     # First add payment methods
     # The controller should return the new payment dict, which the router translates to a 200 response
@@ -238,12 +298,52 @@ def test_get_all_transactions_by_user_id(test_client):
     # All responses should have the same user_id
     user_id = response_1.json()["user_id"]
 
-    # Now try to retrieve payment methods
+    # Now try to retrieve transactions
     get_response = test_client.get(f"/transaction/user_transactions/{user_id}")
     assert get_response.status_code == 200
     data = get_response.json()
     assert len(data) == 2
     assert data[0]["id"] == transaction_id_1
     assert data[1]["id"] == transaction_id_2
+
+def test_get_all_transactions_by_invalid_user_id(test_client):
+    # First add payment methods
+    # The controller should return the new payment dict, which the router translates to a 200 response
+    payment_response_1 = test_client.post("/payment/cash", params={"active": True}, json=cash_payment) 
+    assert payment_response_1.status_code == 200
+    payment_id_1 = payment_response_1.json()["id"]
+
+    transaction_payload_1 = {
+        "payment_method_id": payment_id_1,
+        "order_id": 10,
+        "amount": 25.50
+    }
+
+    # The controller should return the new transaction dict, which the router translates to a 200 response
+    response_1 = test_client.post("/transaction/", json=transaction_payload_1)
+    assert response_1.status_code == 200
+
+    # The controller should return the new payment dict, which the router translates to a 200 response
+    payment_response_2 = test_client.post("/payment/card", params={"active": True}, json=card_payment) 
+    assert payment_response_2.status_code == 200
+    payment_id_2 = payment_response_2.json()["id"]
+
+    transaction_payload_2 = {
+        "payment_method_id": payment_id_2,
+        "order_id": 10,
+        "amount": 25.50
+    }
+
+    # The controller should return the new transaction dict, which the router translates to a 200 response
+    response_2 = test_client.post("/transaction/", json=transaction_payload_2)
+    assert response_2.status_code == 200
+
+    user_id = 999
+
+    # Now try to retrieve transactions should give 404 since no transactions exists for user_id == 999
+    get_response = test_client.get(f"/transaction/user_transactions/{user_id}")
+    assert get_response.status_code == 404
+
+
 
 
