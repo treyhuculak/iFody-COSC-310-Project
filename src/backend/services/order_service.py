4d8813d2d2367676aced5c75539afd9fa1d22f06
LostInHustle <dystopiandice@outlook.com>
2026-04-06 18:01:09 -0700

    Change calculate_order_subtotal so it will check the terms applied for the active offer.

--- src/backend/services/order_service.py
+++ src/backend/services/order_service.py
@@ -1,14 +1,14 @@
 import json
 from src.backend.models.order import OrderCreate
+from src.backend.models.offer import OfferType
 from src.backend.repositories.restaurant_repo import RestaurantRepository
 from src.backend.services.offer_service import OfferService
 from typing import Optional
 
-'''
-This service will handle all the business logic related to orders, such as calculating totals, taxes, and delivery fees. It will interact with the OrderRepository for data persistence and the RestaurantRepository for fetching restaurant-specific information like delivery fees.
-'''
-
 class OrderService:
+    '''
+    This service will handle all the business logic related to orders, such as calculating totals, taxes, and delivery fees. It will interact with the OrderRepository for data persistence and the RestaurantRepository for fetching restaurant-specific information like delivery fees.
+    '''
     TAX_RATES = 'data/tax_rates.json'
 
     def __init__(self):
@@ -18,8 +18,28 @@ class OrderService:
     def calculate_order_subtotal(self, order: OrderCreate):
         total_price = 0
         active_offer = self.offer_service.get_active_offer()
+
         for item in order.order_items:
-            total_price += item.price_at_purchase * item.quantity     
+            total_price += item.price_at_purchase * item.quantity
+        
+        if active_offer:
+            if active_offer["offer_type"] == OfferType.DISCOUNT.value:
+                if active_offer["restaurant_id"] == order.restaurant_id:
+                    total_price *= (1 - (active_offer["discount_value"] / 100))
+            elif active_offer["offer_type"] == OfferType.FREE_ITEM.value:
+                if active_offer["restaurant_id"] == order.restaurant_id:
+                    for free_item_id in active_offer["applied_items"]:
+                        for item in order.order_items:
+                            if item.item_id == free_item_id:
+                                if total_price - item.price_at_purchase > 0:
+                                    total_price -= item.price_at_purchase
+            else:
+                if active_offer["restaurant_id"] == order.restaurant_id:
+                    for applied_item_id in active_offer["applied_items"]:
+                        for item in order.order_items:
+                            if item.item_id == applied_item_id:
+                                total_price -= (item.price_at_purchase - active_offer["price_ceiling"])
+
         return total_price
 
     def calculate_tax(self, order: OrderCreate, order_subtotal: Optional[float] = None):
@@ -32,7 +52,6 @@ class OrderService:
             raise ValueError("Tax rates file not found")
         except json.JSONDecodeError:
             raise ValueError("Error decoding tax rates file")
-
 
     def get_delivery_fee(self, order: OrderCreate):
         restaurant = self.restaurant_repo.get_restaurant_by_id(order.restaurant_id)
