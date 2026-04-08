44c690d46c9909d3a0c20731c7c123af5b22e0c0
LostInHustle <dystopiandice@outlook.com>
2026-04-07 18:53:55 -0700

    Start to commenting out the tests...

--- src/backend/services/order_service.py
+++ src/backend/services/order_service.py
@@ -2,7 +2,7 @@ import json
 from src.backend.models.order import OrderCreate
 from src.backend.models.offer import OfferType
 from src.backend.repositories.restaurant_repo import RestaurantRepository
-from src.backend.services.offer_service import OfferService
+from src.backend.services.offer_service import OfferRepository, OfferService
 from typing import Optional
 
 class OrderService:
@@ -13,7 +13,7 @@ class OrderService:
 
     def __init__(self, temp_weekly_offers: str = None):
         self.restaurant_repo = RestaurantRepository()
-        self.offer_service = OfferService(temp_weekly_offers)
+        self.offer_service = OfferService(OfferRepository(temp_weekly_offers))
 
     def calculate_order_subtotal(self, order: OrderCreate):
         total_price = 0
